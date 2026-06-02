06f4319371ec8ce2d4d625b16fd20bdb3cc2206c
GyandeepPal <gyandeeppal577@gmail.com>
2026-06-02 21:29:59 +0530

    Navbar

---
+++ my-react-app/src/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './App.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/Home">Home</Link>
+      <Link to="/products">About</Link>
+      <Link to="/assignment">Contact</Link>
+    </nav>
+  )
+}
+
+export default Navbar
