00047cad910ad1712750a5475d9dc59fd8cea761
GyandeepPal <gyandeeppal577@gmail.com>
2026-06-13 16:52:25 +0530

    AN32

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -190,23 +190,38 @@
 
 
 
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './Navbar'
-import Home from './Home'
-import About from './About'
-import Cart from './Cart'
+// import React from 'react'
+// import { Routes, Route } from 'react-router-dom'
+// import Navbar from './Navbar'
+// import Home from './Home'
+// import About from './About'
+// import Cart from './Cart'
 
+// const App = () => {
+//   return (
+//     <div>
+//       <Navbar />
+
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/About" element={<About />} />
+//         <Route path="/Cart" element={<Cart />} />
+//       </Routes>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react'
+
+
+import NewsCard from './NewsCard'
 const App = () => {
   return (
     <div>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Cart" element={<Cart />} />
-      </Routes>
+      <NewsCard/>
     </div>
   )
 }
---
+++ my-react-app/src/NewsCard.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+const NewsCard = () => {
+  return (
+    <div className="container">
+      <div className="news-card">
+
+        <div className="tag">EVENING NEWS WRAP</div>
+
+        <h1>
+          IAF AN-32 aircraft crashes in Assam, 5 dead; Delhiites to pay more
+          for electricity from next month & more
+        </h1>
+
+        <div className="footer">
+          <span>13 June 2026</span>
+          <span>• NEWS UPDATE</span>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default NewsCard;
