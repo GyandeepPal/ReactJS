31b559babab86af59c52e6e99e45137f2a71c603
GyandeepPal <gyandeeppal577@gmail.com>
2026-06-14 18:29:33 +0530

    ref

--- my-react-app/src/Rerendring.jsx
+++ my-react-app/src/Rerendring.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer, useRef } from 'react'
 import './App.css'
 const Rerendring = () => {
     const reducer=(state,action)=>{
@@ -6,17 +6,26 @@ const Rerendring = () => {
             return state + 1
         }else if(action==="Gyan Deep"){
             return state - 1
+        }else{
+            return state
         }
     }
  const [count, dispatch] = useReducer(reducer,0)
-   
+   const ref=useRef()
+   useEffect(() => {
+  ref.current.style.background = "red";
+}, []);
+useEffect(() => {
+  ref.current.style.background = "black";
+}, []);
+
   return (
     <div id="Main">
         <h1>CHALO</h1>
         <h3>{count}</h3>
-        <button className='one' onClick={()=>dispatch("Gyan")}>😅</button>
-        <button className='two' onClick={()=>dispatch("Gyan Deep")}>❤️</button>
-        <button className='two' onClick={()=>dispatch("Gyan Deep")}>😃</button>
+        <button ref={ref}className='one' onClick={()=>dispatch("Gyan")}>😅</button>
+        <button ref={ref} className='two' onClick={()=>dispatch("Gyan Deep")}>❤️</button>
+      
     </div>
   )
 }
